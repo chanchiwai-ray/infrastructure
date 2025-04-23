2352dd526882ee963fb9c76e249adc7f6bb23180
Chi Wai Chan <chanchiwairay@gmail.com>
2025-04-23 12:32:12 +0800

    fix: add retries and timeout for wait-for-model-ready.sh
    `juju wait-for model` command polls the status randomly based on the
    database's delta, this can be unrealiable. To work around, retry the
    `juju wait-for model` command, and it will be forced to check the
    database again.

--- shared/hooks/wait-for-model-ready.sh
+++ shared/hooks/wait-for-model-ready.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 MODEL="$1"
+TIMEOUT="${2:-5m0s}"
+RETRIES="${3:-5}"
 
 if [ -z "$MODEL" ]; then
     echo "Wait for the model to be ready."
     echo ""
-    echo "Usage: $0 <MODEL>"
+    echo "Usage: $0 <MODEL> [TIMEOUT=10m0s] [RETRIES=5]"
     exit 1
 fi
 
-juju wait-for model $MODEL --query='forEach(applications, app => app.status == "active")'
+count=0
+while [ $count != $RETRIES ] ; do
+    echo retry counts: $count
+    count=$((count+1))
+    if juju wait-for model $MODEL --timeout=$TIMEOUT --query='forEach(applications, app => app.status == "active")'; then
+        exit 0
+    fi
+done
