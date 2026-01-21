59f5187dff3d02435537e08c96c2a38b420b140f
Ray Chan <chanchiwairay@gmail.com>
2026-01-21 11:31:57 +0800

    chore: update juju provider and k8s version (#8)

--- modules/k8s/main.tf
+++ modules/k8s/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     juju = {
-      version = "~> 0.17.0"
+      version = ">= 1.1.1"
       source  = "juju/juju"
     }
   }
@@ -21,8 +21,8 @@ module "k8s" {
   source = "git::https://github.com/canonical/k8s-operator//charms/worker/k8s/terraform?ref=main"
   count  = var.k8s_units != 0 ? 1 : 0
 
-  units = var.k8s_units
-  model = juju_model.k8s_model.name
+  units      = var.k8s_units
+  model_uuid = juju_model.k8s_model.uuid
 
   base        = var.k8s_base
   channel     = var.k8s_channel
@@ -36,8 +36,8 @@ module "k8s_worker" {
   source = "git::https://github.com/canonical/k8s-operator//charms/worker/terraform?ref=main"
   count  = var.k8s_worker_units != 0 ? 1 : 0
 
-  units = var.k8s_worker_units
-  model = juju_model.k8s_model.name
+  units      = var.k8s_worker_units
+  model_uuid = juju_model.k8s_model.uuid
 
   base        = var.k8s_worker_base
   channel     = var.k8s_worker_channel
@@ -48,8 +48,8 @@ module "k8s_worker" {
 }
 
 resource "juju_integration" "k8s_to_k8s_worker" {
-  model = juju_model.k8s_model.name
-  count = var.k8s_units != 0 && var.k8s_worker_units != 0 ? 1 : 0
+  model_uuid = juju_model.k8s_model.uuid
+  count      = var.k8s_units != 0 && var.k8s_worker_units != 0 ? 1 : 0
 
   application {
     name     = module.k8s[0].app_name
